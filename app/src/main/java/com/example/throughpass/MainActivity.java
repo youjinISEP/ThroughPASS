938537a14504e165cd8dfb3f0d494df7076c0dcc
Leehaewon <banner4@o.cnu.ac.kr>
2020-02-26 11:08:19 +0900

    MainActivity 개발자 이름 주석 추가

--- app/src/main/java/com/example/throughpass/MainActivity.java
+++ app/src/main/java/com/example/throughpass/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * 시작 화면
+ * 이해원, 김유진
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
