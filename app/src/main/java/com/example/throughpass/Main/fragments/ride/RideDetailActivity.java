0ece10f74277cac92d280b5e4956d47d0fb5482d
youjinISEP <jykm0928@gmail.com>
2020-03-03 15:20:21 +0900

    develop ride fragment

---
+++ app/src/main/java/com/example/throughpass/Main/fragments/ride/RideDetailActivity.java
@@ -0,0 +1,31 @@
+package com.example.throughpass.Main.fragments.ride;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.throughpass.R;
+
+public class RideDetailActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ride_detail_fragment);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                finish();
+            }
+        });
+    }
+}
